4331a786d07610ea9cf8722ad9e7f1de868e82f6
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-04-09 14:33:20 +0700

    Update SplashScreen.tsx

--- app/components/SplashScreen.tsx
+++ app/components/SplashScreen.tsx
@@ -1,20 +1,23 @@
+
 "use client";
 
 export default function SplashScreen() {
   return (
-    <div className="fixed inset-0 z-[9999] bg-white flex items-center justify-center">
+    <div className="fixed inset-0 z-[9999] bg-white flex items-center justify-center transition-opacity duration-500">
+      
       <div className="relative w-24 h-24 flex items-center justify-center">
-
+        
         {/* vòng quay */}
-        <div className="absolute inset-0 rounded-full border-4 border-orange-200 border-t-orange-500 animate-spin"></div>
+        <div className="absolute inset-0 rounded-full border-4 border-orange-200 border-t-orange-500 border-r-pink-500 animate-spin"></div>
 
         {/* logo */}
         <img
-          src="/banners/E2377D9D-4E64-4623-A882-B62255CA0114.jpeg"
+          src="/logo.png"
           alt="logo"
-          className="w-12 h-12 object-contain"
+          className="w-12 h-12 object-contain animate-pulse"
         />
       </div>
+
     </div>
   );
 }
